398bf0f5eafdc4009f0309083861f76c763ea883
matteoga95 <gallo.work95@gmail.com>
2022-10-07 15:32:16 +0200

    2 milestone ( presentazione  input e  ticket )

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 
 //attraverso l'evento del click sul bottone calcolo e genero il prezzo finale
-const getPriceButton = document.querySelector("button");
+const getPriceButton = document.getElementById("submit_bt");
 
 getPriceButton.addEventListener("click", function () {
 
     //mi salvo quanti km vuole percorrere dall'input nell'html
-    const numKmpassenger = Number(document.getElementById("numKM").value);
+    const numKmpassenger = Number(document.getElementById("kmPassenger").value);
     //mi salvo quanti anni ha il passeggero dall'input nell'html
-    const numAnniPassenger = Number(document.getElementById("age").value);
-
+    const numAnniPassenger = Number(document.getElementById("agePassenger").value);
+  
     /* Sulla base di queste info devo calcolare i prezzo totale del biglietto , secondo queste regole:
             il prezzo del biglietto è definito in base ai km (0.21€ al km)
             va applicato uno sconto del 20% per i minorenni
@@ -27,8 +27,21 @@ getPriceButton.addEventListener("click", function () {
         prezzoTot = (prezzoTot * 60) / 100;
     }
 
+
+    //modifico i dati del biglietto con quelli inseriti a mano
+    const namePassenger = document.getElementById("namePassenger").value;
+
+  
+    const lastNamePassenger = document.getElementById("lastNamePassenger").value;
+
+    document.getElementById("ticket_full_name").innerHTML= namePassenger + "  " + lastNamePassenger
+
     //l'output del prezzo finale va messo fuori in forma umana ( con max 2 decimali per indicare i centesimi sul prezzo )
-    
+    document.getElementById("ticket_price").innerHTML=prezzoTot.toFixed(2) + " " + "€"
     console.log(prezzoTot.toFixed(2));
 
+
+    //alla fine devo rendere visibile solo il biglietto ( main_content )
+    document.getElementById("input_content").style.display = "none";
+    document.getElementById("main-content").style.display = "block";
 })
